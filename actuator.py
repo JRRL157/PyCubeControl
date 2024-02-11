3170608ab9fe4cbdaaa210c2116f16449b9accd7
JRRL157 <jrrlino157@gmail.com>
2024-02-11 18:10:05 -0300

    Updated

--- actuator.py
+++ actuator.py
@@ -5,17 +5,19 @@ class ReactionWheel:
     def __init__(self, Jm: np.float64 = 0.01, Bm: np.float64 = 0.1, Ke: np.float64 = 0.01,
                  Kt: np.float64 = 0.01, R: np.float64 = 1, L: np.float64 = 0.5,
                  dt: np.float64 = 1e-3):
-        self.Jm = np.array(3*[Jm],dtype=np.float64)
-        self.Bm = np.array(3*[Bm],dtype=np.float64)
-        self.Ke = np.array(3*[Ke],dtype=np.float64)
-        self.Kt = np.array(3*[Kt],dtype=np.float64)
-        self.R = np.array(3*[R],dtype=np.float64)
-        self.L = np.array(3*[L],dtype=np.float64)
+        self.Jm = np.array(3 * [Jm], dtype=np.float64)
+        self.Bm = np.array(3 * [Bm], dtype=np.float64)
+        self.Ke = np.array(3 * [Ke], dtype=np.float64)
+        self.Kt = np.array(3 * [Kt], dtype=np.float64)
+        self.R = np.array(3 * [R], dtype=np.float64)
+        self.L = np.array(3 * [L], dtype=np.float64)
         self.dt = dt
 
         self.domegadt = np.array([0, 0, 0], dtype=np.float64)
         self.current = np.array([0, 0, 0], dtype=np.float64)
         self.omega = np.array([0, 0, 0], dtype=np.float64)
+        self.torque = np.array([0, 0, 0], dtype=np.float64)
+        self.momentum = np.array([0, 0, 0], dtype=np.float64)
         self.angles = np.array([0, 0, 0], dtype=np.float64)
 
     def update(self, Va):
@@ -25,9 +27,22 @@ class ReactionWheel:
 
         for i in range(0, 3):
             dOmegadt[i] = (-self.Bm[i] / self.Jm[i]) * self.omega[i] + (self.Kt[i] / self.Jm[i]) * self.current[i]
-            dCurrentdt[i] = (-self.Ke[i] / self.L[i]) * self.omega[i] + (-self.R[i] / self.L[i]) * self.current[i] + (1 / self.L[i]) * Va[i]
+            dCurrentdt[i] = (-self.Ke[i] / self.L[i]) * self.omega[i] + (-self.R[i] / self.L[i]) * self.current[i] + (
+                        1 / self.L[i]) * Va[i]
 
         self.domegadt = dOmegadt
+        self.torque = self.Jm * self.domegadt
         self.omega += self.dt * self.domegadt
+        self.momentum = self.Jm * self.omega
         self.current += self.dt * dCurrentdt
         self.angles += self.dt * self.omega
+
+
+class Magnetorquer:
+    def __init__(self, N=1000, A=1):
+        self.N = N
+        self.A = A
+
+    def update(self,I,B):
+        self.m = self.N*I*self.A
+        self.torque = self.m * B
--- main.py
+++ main.py
@@ -7,14 +7,12 @@ from controller import PID
 from dynamics import Dynamics
 
 # Constants
-dt = np.float64(0.01)
-Jw = 2.887e-03
-Jb = 2.042e-02
-Jr = np.float64(Jw / Jb)
+dt = np.float64(0.0001)
+J = np.array([[0.002169666666667, 0, 0], [0, 0.002169666666667, 0], [0, 0, 0.002169666666667]])
 
 # Classes declaration
-pid = PID(kp=np.float64(10.0), ki=np.float64(1.0), kd=np.float64(0.05), dt=dt)
-dynamics = Dynamics(jr=Jr, omega=np.array([2, 1, 0.8], dtype=np.float64), dt=dt)
+pid = PID(kp=np.float64(1.0), ki=np.float64(1.0), kd=np.float64(0.0), dt=dt)
+dynamics = Dynamics(J, omega=np.array([3, 1, 2], dtype=np.float64), dt=dt)
 comparator = Comparator(input1_sgn=True, input2_sgn=False)
 reaction_wheel = ReactionWheel(dt=dt)
 
@@ -22,48 +20,81 @@ reaction_wheel = ReactionWheel(dt=dt)
 SP = np.array([0.0, 0.0, 0.0], dtype=np.float64)
 
 n = 0
+m = 1000
 error = []
 rwOmega = []
 cube_omega = []
 time_points = []
+pidOutput = []
 
 while True:
     comparator.compare(SP, dynamics.omega)
     pid.step(comparator.output)
+
+    '''    
+    try:
+        if np.abs(error[-1][0]) <= 1e-2:
+            reaction_wheel.update(np.array([0, 0, 0]))
+            print("N = ", n, " w = ", reaction_wheel.omega)
+        else:
+            reaction_wheel.update(pid.output)
+    except Exception as e:
+        print(e.__traceback__)
+    '''
     reaction_wheel.update(pid.output)
-    dynamics.step(input_velocity=reaction_wheel.omega,
-                  input_acceleration=reaction_wheel.domegadt)
 
-    n += 1
+    dynamics.step(angular_momentum=-reaction_wheel.momentum,
+                  angular_torque=-reaction_wheel.torque,
+                  external_torque=np.array([0, 0, 0]))
 
-    time_points.append(n * dt)
-    error.append(comparator.output)
-    rwOmega.append(reaction_wheel.omega)
-    cube_omega.append(dynamics.omega)
+    if n <= 10:
+        time_points.append(n * dt)
+        error.append(comparator.output)
+        pidOutput.append(pid.output)
+        rwOmega.append(reaction_wheel.omega)
+        cube_omega.append(dynamics.omega)
 
-    if np.abs(error[-1][0]) <= 1e-3:
+    try:
+        if n % 1000 == 0:
+            time_points.append(n * dt)
+            error.append(comparator.output)
+            pidOutput.append(pid.output)
+            rwOmega.append(reaction_wheel.omega)
+            cube_omega.append(dynamics.omega)
+            print("N = ", n, ", error = ", error[-1])
+    except:
+        None
+
+    if np.abs(error[-1][0]) <= 1e-3 or n == 1000000:
         break
 
+    n += 1
 
 # Plot results
 plt.figure(figsize=(10, 6))
 
-plt.subplot(3, 1, 1)
+plt.subplot(4, 1, 1)
 plt.plot(time_points, error, label='Current error of the cubesat angular velocity')
 plt.xlabel('Time (s)')
 plt.ylabel('Error')
 plt.legend()
 
-plt.subplot(3, 1, 2)
+plt.subplot(4, 1, 2)
 plt.plot(time_points, rwOmega, label='RW Omega(rad/s)')
 plt.xlabel('Time (s)')
 plt.ylabel('Omega(rad/s)')
 plt.legend()
 
-plt.subplot(3, 1, 3)
+plt.subplot(4, 1, 3)
 plt.plot(time_points, cube_omega, label='Cubesat Angular velocity')
 plt.xlabel('Time (s)')
 plt.ylabel('Omega')
+plt.legend()
+
+plt.subplot(4, 1, 4)
+plt.plot(time_points, pidOutput, label='Saída de Controle')
+plt.xlabel('Time (s)')
+plt.ylabel('controle')
 plt.legend()
 
 plt.tight_layout()
